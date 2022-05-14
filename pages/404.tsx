de728be5b3f9a9ab0eccb3f97721dffe2dda8af8
hsn6 <hsn6@tuta.io>
2022-05-14 19:13:39 +0430

    Add custom 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import Button from "components/atoms/Button";
+
+const Custom404 = () => {
+  return (
+    <div className="flex flex-col justify-center items-center w-full h-full">
+      <p className="m-4 font-bold text-zinc-100">Not Found!</p>
+      <Link href={"/"}>
+        <Button>&larr;Back to Home</Button>
+      </Link>
+    </div>
+  );
+};
+
+export default Custom404;
