68d0dc5b6bdc3b6f6b38786535316e2010c8858b
hsn6 <hsn6@tuta.io>
2022-05-15 14:53:53 +0430

    Add variants to Button

--- components/atoms/Button.tsx
+++ components/atoms/Button.tsx
@@ -5,21 +5,29 @@ import type {
   PropsWithChildren,
 } from "react";
 
-type ButtonProps = DetailedHTMLProps<
+const variants = {
+  contained:
+    "px-8 py-2 text-lg text-white bg-emerald-600 rounded-md border-none transition-colors cursor-pointer hover:bg-emerald-500 active:bg-emerald-700 font-medium",
+  outlined:
+    "px-8 py-2 text-lg text-white bg-transparent border-emerald-600 border-solid rounded-md transition-colors cursor-pointer hover:bg-emerald-500 active:bg-emerald-700 font-medium",
+};
+
+type BaseButtonProps = DetailedHTMLProps<
   ButtonHTMLAttributes<HTMLButtonElement>,
   HTMLButtonElement
 >;
+interface ButtonProps extends BaseButtonProps {
+  variant?: keyof typeof variants;
+}
 const Button: FC<PropsWithChildren<ButtonProps>> = ({
   children,
+  variant = "contained",
   ...others
 }) => {
   return (
     <button
       {...others}
-      className={
-        "px-8 py-2 text-lg text-white bg-emerald-600 rounded-md border-none transition-colors cursor-pointer hover:bg-emerald-500 active:bg-emerald-700 " +
-        (others.className || "")
-      }
+      className={[variants[variant], others.className || ""].join(" ")}
     >
       {children}
     </button>
